76e2cfb8fbb5c350069f9371d96502985af93dba
Вадим Турко <vadim.turko@gmail.com>
2021-03-29 13:29:00 +0300

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:light_sensor/light_sensor.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _luxString = 'Unknown';
+  LightSensor _light;
+  StreamSubscription _subscription;
+
+  void onData(int luxValue) async {
+    print("Lux value: $luxValue");
+    setState(() {
+      _luxString = "$luxValue";
+    });
+  }
+
+  void stopListening() {
+    _subscription.cancel();
+  }
+
+  void startListening() {
+    _light = new LightSensor();
+    try {
+      _subscription = _light.lightSensorStream.listen(onData);
+    }
+    on LightException catch (exception) {
+      print(exception);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    startListening();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: new Center(
+          child: new Text('Running on: $_luxString\n'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/light_sensor.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:flutter/services.dart';
+
+/// Custom Exception for the plugin,
+/// thrown whenever the plugin is used on platforms other than Android
+class LightException implements Exception {
+  String _cause;
+
+  LightException(this._cause);
+
+  @override
+  String toString() {
+    return _cause;
+  }
+}
+
+class LightSensor {
+  static const EventChannel _eventChannel =
+  const EventChannel("light.eventChannel");
+
+  Stream<int> _lightSensorStream;
+
+  /// Getter for light stream, throws an exception if device isn't on Android platform
+  Stream<int> get lightSensorStream {
+    if (Platform.isAndroid) {
+      if (_lightSensorStream == null) {
+        _lightSensorStream =
+            _eventChannel.receiveBroadcastStream().map((lux) => lux);
+      }
+      return _lightSensorStream;
+    }
+    throw LightException('Light sensor API exclusively available on Android!');
+  }
+}
